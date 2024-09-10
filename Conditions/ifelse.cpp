8e1b85965b84a8eee7e58d7cd14829db276bd207
Pratik Bagale <pratikbagale8@gmail.com>
2024-09-11 01:47:12 +0530

    Add Code

---
+++ Conditions/ifelse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int savings;
+    cin >> savings;
+
+    if (savings > 5000)
+    {
+        cout << "Lokesh\n";
+    }
+    else
+    {
+        cout << "Rahul \n";
+    }
+    return 0;
+}
