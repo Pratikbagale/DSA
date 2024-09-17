397d4777d7d5944a4696ddc00a74ae103cb11488
Pratikbagale <156169569+Pratikbagale@users.noreply.github.com>
2024-09-17 22:19:48 +0530

    Update

---
+++ Q2.cpp
@@ -0,0 +1,20 @@
+// Q2. Give number is odd or even .
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0)
+    {
+        cout << "number is Even " << endl;
+    }
+    else
+    {
+        cout << "number is Odd  " << endl;
+    }
+    return 0;
+}
