3d18ab86c23f467728c999d5035de190727a67a1
Pratik Bagale <pratikbagale8@gmail.com>
2024-09-20 08:22:39 +0530

    Add code

---
+++ SwitchStatement/switch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char button;
+    cout << "Input a character: ";
+    cin >> button;
+
+    switch (button)
+    {
+    case 'a':
+        cout << "Hello" << endl;
+        break;
+
+    case 'b':
+        cout << "Namaste" << endl;
+        break;
+
+    case 'c':
+        cout << "Salut" << endl;
+        break;
+
+    case 'd':
+        cout << "Hola" << endl;
+        break;
+
+    case 'e':
+        cout << "Ciao" << endl;
+        break;
+
+    default:
+        cout << " I am still learning " << endl;
+        break;
+    }
+    return 0;
+}
