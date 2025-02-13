ac4d6953e8bf5632a4d6371b2bd5720200e40385
Pratik Jyotirling Bagale <pratikbagale8@gmail.com>
2025-02-13 20:33:57 +0530

    add code

---
+++ Conditions/nested.cpp
@@ -0,0 +1,22 @@
+// Nested Loop
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int savings;
+    cin >> savings ;
+    
+    if(savings > 5000){
+    if(savings > 1000){
+        cout << "Road trip with friends";
+    } else{
+        cout << "Shopping with Riya";
+    }
+    }else if(savings > 7000){
+        cout << "Jiya\n";
+    } else {
+        cout << "Miku\n";
+    }
+    return 0;
+}
