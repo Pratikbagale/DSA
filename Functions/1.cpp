4cb2e75c184995e041f589f9788ade225d470579
Pratik Jyotirling Bagale <pratikbagale8@gmail.com>
2025-02-16 01:01:49 +0530

    Add code

---
+++ Functions/1.cpp
@@ -0,0 +1,14 @@
+// What is Function
+
+#include <iostream>
+using namespace std;
+
+// function defination
+void printHello(){
+    cout << "Hello World\n";
+}
+
+int main(){
+    printHello();
+    return 0;
+}
