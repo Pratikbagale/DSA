b6392cde3d2567d44691fbb27ea16abd3e123ea7
Pratik Jyotirling Bagale <pratikbagale8@gmail.com>
2025-02-25 23:22:11 +0530

    Add Code

---
+++ Functions/2.cpp
@@ -0,0 +1,17 @@
+// Function Parameters
+
+#include<iostream>
+using namespace std;
+
+// Sum of two numbers
+
+int twoSum(int a, int b){
+    int s = a + b;
+    return s;
+}
+
+int main(){
+
+    cout << twoSum(10, 5) << endl;
+    return 0;
+}
