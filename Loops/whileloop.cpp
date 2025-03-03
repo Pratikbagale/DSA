52a700b89478b8ed742564cbc052c6e97b7082ac
Pratik Jyotirling Bagale <pratikbagale8@gmail.com>
2025-03-03 12:18:50 +0530

    Add code

---
+++ Loops/whileloop.cpp
@@ -0,0 +1,17 @@
+// While loop Print numbers 1 to 5
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 1000;
+    int count = 1;
+
+    while(count <= n){
+        cout << count <<  " ";
+        count++ ;
+    }
+
+    cout << endl;
+    return 0;
+}
